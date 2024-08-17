901ae0bc0f2585328ac3e6642ff81326c3dcbc5b
willyammach <willyam.mach@gmail.com>
2024-08-17 02:33:41 -0300

    adicionar icones a sidebar

---
+++ src/app/includes/nav-buttons/nav-buttons.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatListModule } from '@angular/material/list'; 
+import { GerenciaTamanhoNavbarService } from '../../services/gerencia-tamanho-navbar.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-nav-buttons',
+  standalone: true,
+  imports: [MatIconModule, MatListModule, CommonModule],
+  templateUrl: './nav-buttons.component.html',
+  styleUrl: './nav-buttons.component.scss'
+})
+export class NavButtonsComponent implements OnInit{
+  showMessage: boolean = false;
+  @Input() icon: string = ''
+  @Input() mensagem: string = ''
+  @Input() href: string = ''
+
+  constructor(private gerenciaNavbarService: GerenciaTamanhoNavbarService) {}
+
+  ngOnInit(): void {
+    this.gerenciaNavbarService.isResizeActive.subscribe(value => {
+      this.showMessage = value;
+    })
+  }
+}
--- src/app/includes/sidebar/sidebar.component.ts
+++ src/app/includes/sidebar/sidebar.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { GerenciaTamanhoNavbarService } from '../../services/gerencia-tamanho-navbar.service';
 import { CommonModule } from '@angular/common';
+import { NavButtonsComponent } from '../nav-buttons/nav-buttons.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    NavButtonsComponent
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
